f1d4a16296b0e7062918b595ac1be9ca8479768a
Trey Smith <slicksmith14@gmail.com>
2024-06-12 18:47:00 -0400

    Add some better styling to the book table

--- script.js
+++ script.js
@@ -11,16 +11,19 @@ function Book() {
 function addBookToLibrary() {
     myLibrary.push(textBox.value);
     textBox.value = null;
-    displayBooks();
+    let div = document.createElement('div');
+    div.innerText = myLibrary[myLibrary.length - 1];
+    bookList.append(div);
+    //displayBooks();
     return false;
 }
 
-function displayBooks() {
-    bookList.innerHTML = '';
+// function displayBooks() {
+//     bookList.innerHTML = '';
 
-    for (let i = 0; i < myLibrary.length; i++) {
-        let div = document.createElement('div');
-        div.innerText = myLibrary[i];
-        bookList.append(div);
-    }
-}
+//     for (let i = 0; i < myLibrary.length; i++) {
+//         let div = document.createElement('div');
+//         div.innerText = myLibrary[i];
+//         bookList.append(div);
+//     }
+// }
